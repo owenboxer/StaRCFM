65e1befb017563a457a9720c27120ab8d83c90ad
sandlert <cosmicfauxton@gmail.com>
2017-05-13 11:41:23 -0400

    fix it uper

--- StaRCFM/src/core/Nucleus.java
+++ StaRCFM/src/core/Nucleus.java
@@ -2,14 +2,29 @@ package core;
 
 public class Nucleus {
 
-	double atomicNumber, massNumber;
-
+	public double massNumber;
+	public int atomicNumber;
+	public String element;
 	public Nucleus(String packedNucleus){
 		String subString = "";
 
 		for (int clause = 0; clause < 2; clause++) {
-			subString = packedCard.substring(0, packedCard.indexOf('\t'));
+			subString = packedNucleus.substring(0, packedNucleus.indexOf('\t'));
 			subString.trim();
+			
+			switch(clause){
+			case 0:
+				element = subString;
+				break;
+			case 1:
+				atomicNumber = Integer.parseInt(subString);
+				break;
+			case 2:
+				massNumber = Double.parseDouble(subString);
+				
+			}
+
+			packedNucleus = packedNucleus.substring(packedNucleus.indexOf('\t') + 1);
 		}
 	}
 }
