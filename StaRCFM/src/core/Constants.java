a7dbdbc4c40a4e3b17b1c6c368ae10b5b146ff68
David Owen Carter <davidowencarter@Davids-MacBook-Pro.local>
2017-05-13 09:23:17 -0400

    Updating for real

--- StaRCFM/src/core/Constants.java
+++ StaRCFM/src/core/Constants.java
@@ -1,5 +1,12 @@
 package core;
 
 public enum Constants {
+	ELEMENTARY_CHARGE(1.602217662E-19); //proton and electron charge in Coulombs
 
+
+
+	double value;
+
+	Constants(double value){ this.value = value; }
+	public double getValue(){ return value; }
 }
