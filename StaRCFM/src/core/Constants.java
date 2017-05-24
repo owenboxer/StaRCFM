4bf70868b162781597afd6bcdaffea90a1273bbc
owenboxer <owencarter2000@gmail.com>
2017-05-23 23:18:33 -0400

    Add files via upload

--- StaRCFM/src/core/Constants.java
+++ StaRCFM/src/core/Constants.java
@@ -3,9 +3,8 @@ package core;
 public enum Constants {
 	GRAVITATIONAL_CONSTANT(6.67E-11),
 	HYDROGEN_MASS(1.67E-27),
-	BOLTZMANN_CONSTANT(1.38064852E-23);
-
-
+	BOLTZMANN_CONSTANT(1.38064852E-23),
+	GMC_RATIO(3.685E-15); //Temperature over volume of Giant Molecular Cloud 
 
 	double value;
 
--- StaRCFM/src/core/StaRCFM_Test.java
+++ StaRCFM/src/core/StaRCFM_Test.java
@@ -17,32 +17,46 @@ public class StaRCFM_Test {
 
 		packedNucleus = util.FileHandler.readFile("res/nuclei.txt"); //create nuclei
 		for (int line = 0; line < packedNucleus.length - 1; line++)
-			nucleus[line] = new Nucleus(packedNucleus[line + 1]);
+			nucleus[line] = new Nucleus(packedNucleus[line + 1]);	
 
+		double functionInput = 0;
 		for (int l = 0; l < resolution; l++){
-			starLayer[l] = new StarLayer()
+			starLayer[l] = new StarLayer(starRadius * ((Math.pow(5, l)) / Math.pow(5, resolution - 1)));
+			System.out.println(starRadius * (Math.pow(1.445726, l)) / Math.pow(2, resolution));
 		}
+		calcPressure();
 		runSimulation();
 	}
 	public static void runSimulation(){
-		StarLayer star = new StarLayer();
-		for (time = 0; time < 10000000; time++){
-			star.getRates();
-			star.calcRelativeDensity();
-			star.calcEnergy();
-		}
 	}
 
-	public void calcPressure(){
+	public static void calcPressure(){
 		/* Calculates "initial conditions" pressure based on density, first assuming the star has a uniform density. Then densities are 
 		 * recalculated using previously calculated pressures. This process is repeated until the pressure gradient is at equilibrium with 
 		 * the density gradient.
 		 */
 		double corePressure = (3 * Constants.GRAVITATIONAL_CONSTANT.value * Math.pow(starMass, 2)) / (8 * Math.PI * Math.pow(starRadius, 4));
+		double uniformTemp = 3000;
+		double gravitationalAcceleration[] = new double[resolution];
+		double meanMolarMass = 0.5;
 
-		double pressure[] = new double[resolution];
-		for (int l = 0; l < resolution; l++)
-			pressure[l] = corePressure * Math.exp()
+		for (int l = 0; l < resolution; l++) {
+			starLayer[l].temperature = uniformTemp;
+			starLayer[l].gasPressure = corePressure * Math.exp((-1 * Math.pow(starLayer[l].height, 2) * meanMolarMass * 
+					Constants.HYDROGEN_MASS.value * Constants.GRAVITATIONAL_CONSTANT.value * starMass) / (Math.pow(starRadius, 2) * 
+					Constants.BOLTZMANN_CONSTANT.value * starLayer[l].temperature));
+		}
+
+		for (int t = 0; t < 1000; t++) {
+			for (int l = 0; l < resolution; l++){
+				gravitationalAcceleration[l] = (Constants.GRAVITATIONAL_CONSTANT.value * starLayer[l].height * starLayer[l].gasPressure *
+						meanMolarMass * Constants.HYDROGEN_MASS.value) / (3 * Constants.BOLTZMANN_CONSTANT.value * starLayer[l].temperature);
+				starLayer[l].gasPressure = starLayer[0].gasPressure * Math.exp((-1 * starLayer[l].height * meanMolarMass * 
+						Constants.HYDROGEN_MASS.value * gravitationalAcceleration[l]) / (Constants.BOLTZMANN_CONSTANT.value
+						 * starLayer[l].temperature));
+			}
+			//System.out.println(gravitationalAcceleration[5]);
+		}
 	}
 }
 
